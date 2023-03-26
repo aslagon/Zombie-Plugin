d924881ef8ee1e778e4531d8e6558c18754ee6ec
aslagon <aslagon1234@gmail.com>
2023-03-26 16:09:37 +0900

    start 추가, 랜덤 플레이어 일단 말하게

--- src/main/java/kr/mcv/aslagon/Command.java
+++ src/main/java/kr/mcv/aslagon/Command.java
@@ -6,24 +6,34 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.bukkit.Bukkit.getServer;
+
+
 public class Command implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, org.bukkit.command.@NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (sender instanceof Player) {
-            if (args[0].equals("reload")) {
-                //asd
-                Bukkit.getServer().shutdown();
+        if (command.getName().equalsIgnoreCase("zb")) {
+            if (args.length == 1) {
+                if (args[0].equals("reload")) {
+                    getServer().shutdown();
+                } else if (args[0].equals("start")) {
+                    Player[] players = Bukkit.getOnlinePlayers().toArray(new Player[0]);
+                    Random random = new Random();
+                    Player randomPlayer = players[random.nextInt(players.length)];
+                    getServer().broadcastMessage(String.valueOf(randomPlayer));
+                }
+            } else {
+                commandList((Player) sender);
             }
-
-
-
-
-
         }
-
-
-
-
         return false;
+
+    }
+
+    public void commandList(Player player) {
+        player.sendMessage("/zb reload -> server shutdown and start");
     }
 }
